55a526855514f2465a00a677f760faf788ca116e
x148128 <rana.das@hrblock.com>
2016-11-04 10:45:01 +0000

    Configure Message bundle

--- src/main/java/com/rdas/config/AppConfig.java
+++ src/main/java/com/rdas/config/AppConfig.java
@@ -1,10 +1,20 @@
 package com.rdas.config;
 
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 
 /**
  * Created by x148128 on 03/11/2016.
  */
 @Configuration
 public class AppConfig {
+    @Bean(name ="messageSource")
+    public MessageSource getMessageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("/resources/messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
 }
